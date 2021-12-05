ffccbdb14a77bcfacdf38f3c09696863d8dc0869
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-12-05 23:02:57 +0600

    Review card added with swiper js

--- js/script.js
+++ js/script.js
@@ -33,11 +33,31 @@ menu.addEventListener('click', () => {
   navbar.classList.toggle('active');
 });
 
-videoBtn.forEach(btn =>{
-  btn.addEventListener('click', ()=>{
-      document.querySelector('.controls .active').classList.remove('active');
-      btn.classList.add('active');
-      let src = btn.getAttribute('data-src');
-      document.querySelector('#video-slider').src = src;
+videoBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    document.querySelector('.controls .active').classList.remove('active');
+    btn.classList.add('active');
+    let src = btn.getAttribute('data-src');
+    document.querySelector('#video-slider').src = src;
   });
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
